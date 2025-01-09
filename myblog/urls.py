1b5fec3bd1296985667ae74d7e8ac779c4026a92
sumaya <sumaya.hakeem@dci-student.org>
2025-01-09 09:44:28 +0100

    post exercise

--- myblog/urls.py
+++ myblog/urls.py
@@ -22,5 +22,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('posts/', include('posts.urls')),
+    path('/posts/', include('posts.urls')),
 ]
